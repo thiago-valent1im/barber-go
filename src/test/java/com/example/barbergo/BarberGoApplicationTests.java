c4f8cd822b8c82660caf77769faf707859032bcd
thiago-valent1im <juvenalvalentim10@gmail.com>
2026-04-06 18:05:54 -0300

    init

---
+++ src/test/java/com/example/barbergo/BarberGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barber_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
